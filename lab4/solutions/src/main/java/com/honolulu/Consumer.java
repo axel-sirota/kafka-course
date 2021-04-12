9c834f13013402e96ba6a51c15cbf562193fe598
Axel Sirota <axel.sirota@gmail.com>
2021-04-12 18:18:23 -0300

    lab4

---
+++ lab4/solutions/src/main/java/com/honolulu/Consumer.java
@@ -0,0 +1,44 @@
+package com.honolulu;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.DoubleDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker-1:9092,broker-2:9093");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, args[0] + ".consumer");
+
+        KafkaConsumer<String, Double> consumer = new KafkaConsumer<>(props);
+
+        Thread haltedHook = new Thread(consumer::close);
+        Runtime.getRuntime().addShutdownHook(haltedHook);
+
+        consumer.subscribe(Collections.singletonList(args[0]));
+
+        while (true) {
+            ConsumerRecords<String, Double> records = consumer.poll(Duration.ofMillis(100));
+            records.forEach(record -> processRecord(record.value()));
+        }
+    }
+
+    private static void processRecord(Double hurricane) {
+        log.info("New alert received of intensity: " + hurricane);
+    }
+}
